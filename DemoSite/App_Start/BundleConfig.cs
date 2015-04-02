6e6931faf94aa446dbfa06672339f87fda918b11
wmbt <ben.overlord@gmail.com>
2015-04-02 22:17:16 +0300

    Старт

---
+++ DemoSite/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace DemoSite
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear();
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-1.9.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapjs").Include(
+                        "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include(
+                        "~/Content/bootstrap.min.css", "~/Content/jumbotron-narrow.css"));
+            
+            bundles.Add(new StyleBundle("~/Content/commoncss").Include(
+             "~/Content/bootstrap.min.css", "~/Content/common.css"));
+        }
+    }
+}
---
+++ DemoSite/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            string path = HttpContext.Server.MapPath("~/Data/data.csv");
+            var strings = System.IO.File.ReadAllLines(path, Encoding.GetEncoding(1251))
+                .Select(x => x.Split(';').ToArray()).ToArray();
+            ViewData["Cells"] = strings;
+            
+            return View();
+        }
+        public ActionResult Titan() 
+        {
+            return View();
+        }
+
+        public ActionResult About() 
+        {
+            return View();
+        }
+    }
+}
