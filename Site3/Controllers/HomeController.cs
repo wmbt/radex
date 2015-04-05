8a3a65f31dd4eb745db00b05c19eb66863f17480
wmbt <ben.overlord@gmail.com>
2015-04-05 19:02:00 +0300

    Готов 3й сайт

--- Site3/Controllers/HomeController.cs
+++ Site3/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DataAccess;
+using Site3.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +14,24 @@ namespace Site3.Controllers
         //
         // GET: /Home/
 
-        public ActionResult Index()
+        public ActionResult Index(string colName = "id", string order = "asc")
+        {
+            var cs = ConfigurationManager.ConnectionStrings["DefaultDb"];
+            var repo = new Repository(cs.ConnectionString);
+
+            return View(new PriceTable
+            {
+                Items = repo.GetItems("SITE3_PRICE", x => new TableItem(x), colName + " " + order).ToArray(),
+                SortColumn = colName,
+                SortOrder = order
+            });
+        }
+        public ActionResult Info()
+        {
+            return View();
+        }
+
+        public ActionResult About()
         {
             return View();
         }
---
+++ Site3/Models/TableItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Site3.Models
+{
+    public class TableItem
+    {
+        public int Id { get; private set; }
+        public string Size { get; set; }
+        public string SteelMark { get; set; }
+        public double Price { get; set; }
+        public string Note { get; set; }
+        
+        public TableItem(IDataRecord r) 
+        {
+            Id = (int)r["ID"];
+            Size = (string)r["SIZE"];
+            SteelMark = (string)r["STEEL_MARK"];
+            Price = (double)r["PRICE"];
+            Note = (string)r["NOTE"];
+        }
+    }
+
+    public class PriceTable 
+    {
+        public TableItem[] Items { get; set; }
+        public string SortColumn { get; set; }
+        public string SortOrder { get; set; }
+    }
+}
