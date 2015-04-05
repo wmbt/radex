494b6053e1b3c40ab82533033367ed6aefa56249
wmbt <ben.overlord@gmail.com>
2015-04-05 15:44:48 +0300

    2 сайта

--- Site2/App_Start/RouteConfig.cs
+++ Site2/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace Site2
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "OnlyAction",
+                "{action}",
+                new { controller = "Home", action = "Index" }
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ Site2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using DataAccess;
+using Site2.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index(string colName = "id", string order = "asc")
+        {
+            var cs = ConfigurationManager.ConnectionStrings["DefaultDb"];
+            var repo = new Repository(cs.ConnectionString);
+
+            return View(new PriceTable
+            {
+                Items = repo.GetItems("SITE2_PRICE", x => new TableItem(x), colName + " " + order).ToArray(),
+                SortColumn = colName,
+                SortOrder = order
+            });
+            
+        }
+        public ActionResult Info()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
